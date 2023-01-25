bbb4998f52efdc3592136c910bc87588da3ffd01
ShabanovVA <1514768zp@gmail.com>
2023-01-25 21:28:00 +0200

    Перші 2 завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+function timeUpdateValue(event) {
+    localStorage.setItem("videoplayer-current-time", event.seconds)
+};
+
+player.on('timeupdate',
+    throttle(timeUpdateValue, 1000));
+
+const currentTime = localStorage.getItem("videoplayer-current-time");
+player.setCurrentTime(currentTime);
